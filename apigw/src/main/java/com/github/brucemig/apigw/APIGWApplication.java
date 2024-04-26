a01e2d56947c6ff47f34724a15f01a1ba983c840
bruce-mig <brucetmig@icloud.com>
2024-04-26 10:03:53 +0200

    added API Gateway

---
+++ apigw/src/main/java/com/github/brucemig/apigw/APIGWApplication.java
@@ -0,0 +1,13 @@
+package com.github.brucemig.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGWApplication.class, args);
+	}
+
+}
