7521d08672e2fc6d67b6fb2ad59201dd4caf3396
bruce-mig <brucetmig@icloud.com>
2024-07-11 11:42:57 +0200

    normalised all line endings to LF

--- clients/src/test/java/com/github/brucemig/clients/ClientsApplicationTests.java
+++
@@ -1,1 +0,0 @@
-package com.github.brucemig.clients;import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;@SpringBootTestclass ClientsApplicationTests {	@Test	void contextLoads() {	}}
--- customer/src/main/java/com/github/brucemig/customer/CustomerService.java
+++ customer/src/main/java/com/github/brucemig/customer/CustomerService.java
@@ -1,1 +1,46 @@
-package com.github.brucemig.customer;import com.github.brucemig.amqp.RabbitMQMessageProducer;import com.github.brucemig.clients.fraud.FraudCheckResponse;import com.github.brucemig.clients.fraud.FraudClient;import com.github.brucemig.clients.notification.NotificationRequest;import org.springframework.stereotype.Service;@Servicepublic record CustomerService(CustomerRepository customerRepository,                              FraudClient fraudClient,                              RabbitMQMessageProducer rabbitMQMessageProducer                              ) {    public void registerCustomer(CustomerRegistrationRequest request) {        Customer customer = Customer.builder()                .firstName(request.firstName())                .lastName(request.lastName())                .email(request.email())                .build();        // todo: check if email valid        // todo: check if email not taken        customerRepository.saveAndFlush(customer);        // todo: check if fraudster        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());        if (fraudCheckResponse.isFraudster()) {            throw new IllegalStateException("fraudster");        }        NotificationRequest notificationRequest = new NotificationRequest(                customer.getId(),                customer.getEmail(),                String.format("Hi %s, welcome to Bruce's fraud-detection microservices", customer.getFirstName())        );        rabbitMQMessageProducer.publish(                notificationRequest,                "internal.exchange",                "internal.notification.routing-key"                );    }}
+package com.github.brucemig.customer;
+
+import com.github.brucemig.amqp.RabbitMQMessageProducer;
+import com.github.brucemig.clients.fraud.FraudCheckResponse;
+import com.github.brucemig.clients.fraud.FraudClient;
+import com.github.brucemig.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final FraudClient fraudClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Bruce's fraud-detection microservices", customer.getFirstName())
+        );
+
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
+                );
+
+    }
+}
