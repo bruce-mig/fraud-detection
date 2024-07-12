e6dffee5c5168dd87d1b2c16d54f80ed2d0f9d6b
bruce-mig <brucetmig@icloud.com>
2024-07-12 10:49:44 +0200

    normalised all line endings to LF

--- customer/src/main/java/com/github/brucemig/customer/CustomerApplication.java
+++ customer/src/main/java/com/github/brucemig/customer/CustomerApplication.java
@@ -1,1 +1,22 @@
-package com.github.brucemig.customer;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.openfeign.EnableFeignClients;@SpringBootApplication(		scanBasePackages = {				"com.github.brucemig.customer",				"com.github.brucemig.amqp",		})@EnableFeignClients(		basePackages = "com.github.brucemig.clients")public class CustomerApplication {	public static void main(String[] args) {		SpringApplication.run(CustomerApplication.class, args);	}}
+package com.github.brucemig.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.github.brucemig.customer",
+				"com.github.brucemig.amqp",
+		}
+)
+@EnableFeignClients(
+		basePackages = "com.github.brucemig.clients"
+)
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
--- customer/src/main/java/com/github/brucemig/customer/CustomerRegistrationRequest.java
+++ customer/src/main/java/com/github/brucemig/customer/CustomerRegistrationRequest.java
@@ -1,1 +1,8 @@
-package com.github.brucemig.customer;public record CustomerRegistrationRequest(        String firstName,        String lastName,        String email) {}
+package com.github.brucemig.customer;
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email
+) {
+}
